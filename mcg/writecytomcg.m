0b2f7fab9dcb455e7cd587bbfa0d1b5d9a2f92be
striant <sof.triantafillou@gmail.com>
2016-02-09 11:50:01 +0200

    added writecytomg, corrected some bugs

---
+++ mcg/writecytomcg.m
@@ -0,0 +1,22 @@
+function [] = writecytomcg(mcg, fileName, headers)
+fid = fopen(fileName, 'w');
+
+nVars = length(mcg);
+if nargin==2
+    headers = strsplit(sprintf('X%d ', 1:10), ' ');
+    headers = headers(1:nVars);
+end
+
+symbolsXY = {'o', '>', '-', 'z'};
+symbolsYX = {'o', '<', '-', 'z'};
+fprintf(fid, 'Source Interaction Target\n');
+for X =1:nVars
+    for Y =X+1:nVars
+        if mcg(X, Y) 
+            fprintf(fid, '%s %s-%s %s\n', headers{X}, symbolsYX{mcg(Y, X)},symbolsXY{mcg(X, Y)}, headers{Y});
+        end
+    end
+end
+
+fclose(fid);
+end
